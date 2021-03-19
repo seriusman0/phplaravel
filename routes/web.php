b747f2eea51ccefb32c89f00b1b8db4c64a853c1
serius ubuntu <seriusman0@gmail.com>
2021-03-19 16:02:08 +0700

    first time

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [HomeController::class, 'home']);
+// Route::get('/about', [HomeController::class, 'about']);
+// Route::get('/contact', [HomeController::class, 'contact']);
+// Route::get('/products', [HomeController::class, 'products']);
+
+// Route::get('/', 'HomeController@home')->name('home');
+Route::get('/', 'HomeController@about')->name('about');
+Route::get('/', 'HomeController@contact')->name('contact');
+Route::get('/', 'HomeController@products')->name('products');
